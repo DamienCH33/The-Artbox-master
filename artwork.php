25c1474e73352c62a9fea7de7bdd38d1b3dd139e
DamienCH33 <damienchauveau@hotmail.fr>
2025-05-02 15:56:16 +0200

    MAJ gestion erreur artwork.php

--- artwork.php
+++ artwork.php
@@ -3,40 +3,35 @@ include_once('header.php');
 include_once('database.php');
 $db = connection();
 
-if (empty($_GET['id'])) {
+if (empty($_GET['id']) || 0 === $id = (int) $_GET['id']) {
     header('location: index.php');
-    exit;
-}
-
-$id = (int) $_GET['id'];
-if ($id === 0) {
-    echo 'Aucun id trouvé.';
     exit;
 }
 
 $sqlrequest = 'SELECT * FROM artworks WHERE id = ?';
 $artstatement = $db->prepare($sqlrequest);
-$artstatement->execute(params: [$id]);
+$artstatement->execute([$id]);
 $art = $artstatement->fetch();
 
+if (!$art) {
+    header('location: index.php');
+    exit;
+}
 ?>
 
 <main>
-    <?php if ($art) { ?>
-        <article id="detail-oeuvre">
-            <div id="img-oeuvre">
-                <img src="<?php echo $art['image'] ?>" alt="<?php echo $art['title'] ?>">
-            </div>
-            <div id="contenu-oeuvre">
-                <h1><?php echo $art['title'] ?></h1>
-                <p class="description"><?php echo $art['artist'] ?></p>
-                <p class="description-complete">
-                    <?php echo $art['description'] ?>
-                </p>
-            </div>
-        </article>
-    <?php } else { ?>
-        <p class="error">L'œuvre demandée n'existe pas.</p>
-   <?php } ?>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="<?php echo $art['image'] ?>" alt="<?php echo $art['title'] ?>">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1><?php echo $art['title'] ?></h1>
+            <p class="description"><?php echo $art['artist'] ?></p>
+            <p class="description-complete">
+                <?php echo $art['description'] ?>
+            </p>
+        </div>
+    </article>
 </main>
+
 <?php include_once('footer.php'); ?>
